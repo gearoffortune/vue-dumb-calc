e8d6a57d062ab8396f387e9be97d7642454d0137
gearoffortune <gearoffortune@DESKTOP-PE6VDSL.localdomain>
2020-08-17 13:03:09 +0300

    finished writing calculator state machine

--- calcMachine.js
+++ calcMachine.js
@@ -1,73 +1,141 @@
-let {Machine, interpret} = require('xstate');
+let {Machine, interpret, assign} = require('xstate');
 
 
-const numInputMachine = Machine({
-    initial: 'allNil',
-    states: {
-        allNil: {
-            on: {ZERO: 'allNil' , NON_ZERO: 'nonAllNil'} 
-        },
-        nonAllNil: {
-            on: {ZERO: 'nonAllNil', NON_ZERO: 'nonAllNil'}
+const actionSet = {
+    addNumToFirstOperand: assign({
+        firstOperand: (context, event) => {
+            return context.firstOperand === '0' ? event.number : context.firstOperand + event.number;
         }
+    }),
+    setOperator: assign({
+        operator: (_, event) => {
+            return event.operator;
+        }
+    }),
+    setResultAsOperand: assign({
+        firstOperand: (context) =>{
+            return context.result;
+        },
+        result: () => {
+            return 0;
+        }
+    }),
+    addNumToSecondOperand: assign({
+        secondOperand: (context, event) => {
+            return context.secondOperand === '0' ? event.number : context.secondOperand + event.number;
+        }
+    }),
+    calculateResult: assign({
+        result: (context) => {
+            return context.operatorSet[context.operator](((-1)**Number(context.firstOperandNegative))*Number(context.firstOperand), Number(context.secondOperand))
+        }
+    })
+}
+const calcContext = {
+    firstOperand: '0',
+    firstOperandNegative: false,
+    operator: undefined,
+    secondOperand: '0',
+    secondOperandNegative: false,
+    result: 0,
+    operatorSet: {
+        '+': (a, b) => a + b,
+        '-': (a, b) => a - b,
+        '*': (a, b) => a * b,
+        '/': (a, b) => a / b
     }
-})
+}
 
 const calcMachine = Machine({
     id: 'calculator',
     initial: 'start',
     states: {
         start: {
-            on: { NUMBER : 'firstOperand', MINUS: 'firstNegative' }
+            on: { 
+                NUMBER : {
+                    target: 'firstOperand', 
+                    cond: 'isNonNil',
+                    actions: 'addNumToFirstOperand'
+                },
+                MINUS: 'firstNegative' 
+            }
         },
         firstNegative: {
-            on: {NUMBER: 'firstOperand'}
+            on: {
+                NUMBER: {
+                    target: 'firstOperand',
+                    cond: 'isNonNil',
+                    actions: 'addNumToFirstOperand'
+                }
+            }
         },
         firstOperand: {
-            on: {NUMBER: 'firstOperand', OPERATOR: 'operator'},
-        initial: 'allNil',
-            states: {
-                allNil: {
-                    on: {ZERO: 'allNil' , NON_ZERO: 'nonAllNil'} 
+            on: {
+                NUMBER: {
+                    target: 'firstOperand', 
+                    actions: 'addNumToFirstOperand'
                 },
-                nonAllNil: {
-                    on: {ZERO: 'nonAllNil', NON_ZERO: 'nonAllNil'}
+                OPERATOR: {
+                    target: 'operator',
+                    guards: 'isOperator',
+                    actions: 'setOperator'
                 }
-    }
+            },
         },
         operator: {
-            on: {NUMBER: 'secondOperand', OPERATOR: 'operator', MINUS: 'secondNegative'}
-        },
-        secondNegative: {
-            on: {NUMBER: 'secondOperand'}
+            on: {
+                NUMBER: {
+                    target: 'secondOperand',
+                    cond: 'isNonNil',
+                    actions: 'addNumToSecondOperand'
+                }, 
+                OPERATOR: {
+                    target: 'operator', 
+                    cond: 'isOperator'
+                }
+            }
         },
         secondOperand: {
-            on: {EQUALS: 'result'},
-        initial: 'allNil',
-            states: {
-                allNil: {
-                    on: {ZERO: 'allNil' , NON_ZERO: 'nonAllNil'} 
+            on: {
+                EQUALS: {
+                target: 'result',
+                actions: 'calculateResult'
                 },
-                nonAllNil: {
-                    on: {ZERO: 'nonAllNil', NON_ZERO: 'nonAllNil'}
+                NUMBER: {
+                    target: 'secondOperand',
+                    actions: 'addNumToSecondOperand'
                 }
-    }
+        },
         },
         result: {
-            on: {OPERATOR: 'operator'}
+            on: {
+                OPERATOR: {
+                    target: 'operator',
+                    actions: 'setResultAsOperand',
+                    cond: 'isOperator'
+                }
+            }
         }
-    }
+    },
+    context: {...calcContext}
+},
+{
+    guards: {
+        isNonNil(_, event) {
+            return event.number && event.number !== 0;
+        },
+        isOperator(_, event) {
+            return !!event.operator;
+        }
+    },
+    actions: { ...actionSet }
 })
 
 const calc = interpret(calcMachine)
-.onTransition((state) => console.log(state.value))
+.onTransition((state) => console.log(state.context))
 .start();
 
-calc.send('NUMBER');
-calc.send('ZERO');
-calc.send('ZERO');
-calc.send('ZERO');
-calc.send('NON_ZERO');
-calc.send('OPERATOR');
-calc.send('NUMBER');
-calc.send('NON_ZERO');
+calc.send({type: 'NUMBER', number: "9"});
+calc.send({type: 'OPERATOR', operator: "-"});
+calc.send({type: 'NUMBER', number: "3"});
+calc.send('EQUALS');
